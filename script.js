89fcf43292699d43a85ac1687ce1537301efad0c
Austin Luper <austin.luper@gmail.com>
2023-11-27 21:11:28 -0500

    Enemy AI decision-making added to script.js

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ let youWin = alert("You win!")
 let youTied = alert("You Tied!")
 
 let youLose = alert("You Lose!")
-    
 
-var enemyArray = [
-    "R",
-    "P",
-    "S",
+function enemyChoice() {
+    var enemyArray = [
+        "R",
+        "P",
+        "S",
 ];
-
-var randomItem = 
-enemyArray[Math.floor(Math.random()*enemyArray.length)]
-
+    var randomItem = enemyArray[Math.floor(Math.random()*enemyArray.length)]
+    var decision = enemyArray[randomItem]
+    return decision;
+}
